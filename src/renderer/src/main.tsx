939a68f0f778a503d3010132742592fa7e217979
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-07-21 15:08:43 +0500

    style: load board visual polish layer

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,6 +3,7 @@ import './assets/rich-text-quote.css'
 import './assets/focus-mode.css'
 import './assets/light-theme.css'
 import './assets/light-theme-components.css'
+import './assets/board-visual-polish.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
