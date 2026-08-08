b2e711c65e6d230176940c2b7c1673d02ce1816d
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-08-08 14:06:53 +0500

    Define finance report analytics contract

---
+++ src/shared/contracts/finance-report-analytics.ts
@@ -0,0 +1,42 @@
+import type {
+  FinanceReport,
+  FinanceTransferFlow
+} from './finance'
+
+export interface FinanceReportAccountActivity {
+  accountId: string
+  accountName: string
+  currencyCode: string
+  incomeMinor: number
+  expenseMinor: number
+  transferInMinor: number
+  transferOutMinor: number
+  netMinor: number
+  operationCount: number
+}
+
+export interface FinanceReportTransferFlow extends FinanceTransferFlow {
+  convertedAmountMinor: number | null
+}
+
+export type FinanceReportAnalytics = Omit<FinanceReport, 'transferFlows'> & {
+  incomeCount: number
+  expenseCount: number
+  transferCount: number
+  averageIncomeMinor: number
+  averageDailyExpenseMinor: number
+  transferVolumeMinor: number
+  savingsRatePercent: number | null
+  balanceStartMinor: number | null
+  balanceEndMinor: number | null
+  balanceChangeMinor: number | null
+  comparisonIncomeMinor: number
+  comparisonExpenseMinor: number
+  comparisonNetMinor: number
+  incomeChangePercent: number | null
+  expenseChangePercent: number | null
+  netChangePercent: number | null
+  comparisonMissingRateCurrencies: string[]
+  accountActivity: FinanceReportAccountActivity[]
+  transferFlows: FinanceReportTransferFlow[]
+}
