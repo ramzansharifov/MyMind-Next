b60b7f7039878e48c493e94e04a9a946a9eb1c6b
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-08-08 13:35:53 +0500

    Remove account colors from limit dialog fixtures

--- src/renderer/src/modules/finance/components/dialogs/FinanceLimitDialog.test.tsx
+++ src/renderer/src/modules/finance/components/dialogs/FinanceLimitDialog.test.tsx
@@ -24,7 +24,6 @@ const cash: FinanceAccountSummary = {
   initialBalanceMinor: 0,
   balanceMinor: 0,
   icon: 'banknote',
-  color: '#34d399',
   transactionCount: 0,
   lastTransactionAt: null,
   periodChangeMinor: 0,
@@ -36,8 +35,7 @@ const card: FinanceAccountSummary = {
   ...cash,
   id: 'card',
   name: 'Карта',
-  icon: 'credit-card',
-  color: '#60a5fa'
+  icon: 'credit-card'
 }
 
 const usd: FinanceAccountSummary = {
@@ -45,8 +43,7 @@ const usd: FinanceAccountSummary = {
   id: 'usd',
   name: 'Доллары',
   currencyCode: 'USD',
-  icon: 'wallet',
-  color: '#a78bfa'
+  icon: 'wallet'
 }
 
 const expenseTag: FinanceTagSummary = {
