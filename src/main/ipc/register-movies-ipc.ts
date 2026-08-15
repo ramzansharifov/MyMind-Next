003ac093b1b2411ca48824902f8cca071d1d9532
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-08-15 23:09:02 +0500

    Add movies IPC handlers

---
+++ src/main/ipc/register-movies-ipc.ts
@@ -0,0 +1,37 @@
+import { ipcMain } from 'electron'
+
+import { MOVIES_IPC_CHANNELS } from '../../shared/contracts/movies'
+import {
+  createMovieInputSchema,
+  deleteMovieInputSchema,
+  getMovieInputSchema,
+  updateMovieInputSchema
+} from '../../shared/validation/movies'
+import {
+  createMovie,
+  deleteMovie,
+  getMovie,
+  listMoviesOverview,
+  updateMovie
+} from '../repositories/movies.repository'
+import { mainOperationTracker } from '../services/main-operation-tracker'
+
+export function registerMoviesIpcHandlers(): void {
+  Object.values(MOVIES_IPC_CHANNELS).forEach((channel) => ipcMain.removeHandler(channel))
+
+  ipcMain.handle(MOVIES_IPC_CHANNELS.listOverview, () =>
+    mainOperationTracker.run(() => listMoviesOverview())
+  )
+  ipcMain.handle(MOVIES_IPC_CHANNELS.getMovie, (_event, rawInput: unknown) =>
+    mainOperationTracker.run(() => getMovie(getMovieInputSchema.parse(rawInput)))
+  )
+  ipcMain.handle(MOVIES_IPC_CHANNELS.createMovie, (_event, rawInput: unknown) =>
+    mainOperationTracker.run(() => createMovie(createMovieInputSchema.parse(rawInput)))
+  )
+  ipcMain.handle(MOVIES_IPC_CHANNELS.updateMovie, (_event, rawInput: unknown) =>
+    mainOperationTracker.run(() => updateMovie(updateMovieInputSchema.parse(rawInput)))
+  )
+  ipcMain.handle(MOVIES_IPC_CHANNELS.deleteMovie, (_event, rawInput: unknown) =>
+    mainOperationTracker.run(() => deleteMovie(deleteMovieInputSchema.parse(rawInput)))
+  )
+}
