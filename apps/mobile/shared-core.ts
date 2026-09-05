364c855c5e3395aa6db2559d54f54a27ef10d3e6
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-09-05 23:43:16 +0500

    feat: bootstrap shared core in mobile app

---
+++ apps/mobile/shared-core.ts
@@ -0,0 +1,5 @@
+import { createCoreContext } from '@mymind/core'
+import { designTokens } from '@mymind/design'
+
+export const mobileCoreContext = createCoreContext('mobile')
+export const mobileDesignTokens = designTokens
