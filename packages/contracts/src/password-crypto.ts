02b631789f062ac3210dc0485245c8b6355858b5
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-09-07 07:11:50 +0500

    feat: define password crypto port

---
+++ packages/contracts/src/password-crypto.ts
@@ -0,0 +1,34 @@
+export interface PasswordScryptParameters {
+  n: number
+  r: number
+  p: number
+  keyLength: number
+  maxMemoryBytes: number
+}
+
+export interface PasswordEncryptedPayload {
+  nonce: Uint8Array
+  ciphertext: Uint8Array
+  tag: Uint8Array
+}
+
+export interface PasswordCryptoPort {
+  randomBytes(length: number): Uint8Array
+  randomInt(maxExclusive: number): number
+  deriveScryptKey(
+    password: string,
+    salt: Uint8Array,
+    parameters: PasswordScryptParameters
+  ): Promise<Uint8Array>
+  encryptAes256Gcm(
+    plaintext: Uint8Array,
+    key: Uint8Array,
+    aad: Uint8Array
+  ): PasswordEncryptedPayload
+  decryptAes256Gcm(
+    payload: PasswordEncryptedPayload,
+    key: Uint8Array,
+    aad: Uint8Array
+  ): Uint8Array
+  timingSafeEqual(left: Uint8Array, right: Uint8Array): boolean
+}
