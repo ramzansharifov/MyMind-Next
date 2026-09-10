8679d9e3faf9296d1061169588a04e09e27acdeb
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-09-10 22:13:35 +0500

    refactor: separate native and web bootstraps

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -1,26 +1,7 @@
-import { registerRootComponent } from 'expo'
 import { Platform } from 'react-native'
-import { getSafeWebPreviewRedirect } from './src/shared/platform/web-preview'
 
-function registerNativeApp(): void {
-  const App = require('./App').default as typeof import('./App').default
-  registerRootComponent(App)
-}
-
-if (Platform.OS !== 'web') {
-  registerNativeApp()
+if (Platform.OS === 'web') {
+  void import('./src/bootstrap/web')
 } else {
-  const browserLocation = typeof window === 'undefined' ? undefined : window.location
-  const webPreviewRedirect = getSafeWebPreviewRedirect(
-    browserLocation?.href,
-    process.env.EXPO_PUBLIC_MYMIND_WEB_PREVIEW_ORIGIN
-  )
-
-  if (browserLocation && webPreviewRedirect) {
-    browserLocation.replace(webPreviewRedirect)
-  } else {
-    void import('./App').then(({ default: App }) => {
-      registerRootComponent(App)
-    })
-  }
+  require('./src/bootstrap/native')
 }
