2f0490a7236454d7dd58613233858d1b175ddb8b
Ramzan Sharifov <114869338+ramzansharifov@users.noreply.github.com>
2026-09-26 22:06:55 +0500

    feat: define music JSON upsert contract

--- packages/contracts/src/music.ts
+++ packages/contracts/src/music.ts
@@ -78,6 +78,31 @@ export interface CreateMusicItemsInput {
   items: CreateMusicItemInput[]
 }
 
+export interface UpsertMusicItemInput extends CreateMusicItemInput {
+  id?: string | null
+}
+
+export interface UpsertMusicPlaylistInput {
+  id?: string | null
+  name: string
+  coverUrl?: string | null
+  trackIds: string[]
+}
+
+export interface UpsertMusicLibraryInput {
+  items: UpsertMusicItemInput[]
+  playlists: UpsertMusicPlaylistInput[]
+}
+
+export interface UpsertMusicLibraryResult {
+  items: MusicItemRecord[]
+  playlists: MusicPlaylistRecord[]
+  createdItems: number
+  updatedItems: number
+  createdPlaylists: number
+  updatedPlaylists: number
+}
+
 export interface GetMusicItemInput {
   id: string
 }
@@ -115,6 +140,7 @@ export const MUSIC_IPC_CHANNELS = {
   getItem: 'music:get-item',
   createItem: 'music:create-item',
   createItems: 'music:create-items',
+  upsertLibrary: 'music:upsert-library',
   updateItem: 'music:update-item',
   deleteItem: 'music:delete-item',
   createPlaylist: 'music:create-playlist',
@@ -129,6 +155,7 @@ export interface MusicApi {
   getItem(input: GetMusicItemInput): Promise<MusicItemRecord | null>
   createItem(input: CreateMusicItemInput): Promise<MusicItemRecord>
   createItems(input: CreateMusicItemsInput): Promise<MusicItemRecord[]>
+  upsertLibrary(input: UpsertMusicLibraryInput): Promise<UpsertMusicLibraryResult>
   updateItem(input: UpdateMusicItemInput): Promise<MusicItemRecord>
   deleteItem(input: DeleteMusicItemInput): Promise<boolean>
   createPlaylist(input: CreateMusicPlaylistInput): Promise<MusicPlaylistRecord>
